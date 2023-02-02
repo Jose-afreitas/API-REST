25e6f59c9b6fe10c5288250d0d92a571b6745bfe
Jose-afreitas <josealmeida106@gmail.com>
2023-02-02 16:55:25 -0300

    wip installing bcryptjs

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
 
 export default class Aluno extends Model {
   static init(sequelize) {
@@ -43,6 +44,11 @@ export default class Aluno extends Model {
         sequelize,
       },
     );
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
     return this;
   }
 }
